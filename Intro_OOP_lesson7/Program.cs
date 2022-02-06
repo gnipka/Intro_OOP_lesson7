e09d655c504333d9747edc0f3abc8618b42cb163
gnipka <gnipka29@gmail.com>
2022-02-06 13:51:17 +0300

    Добавьте файлы проекта.

---
+++ Intro_OOP_lesson7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intro_OOP_lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
